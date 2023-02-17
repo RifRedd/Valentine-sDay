2e9fe7d30f0d8918f801612149f1362f3f45494f
RifRedd <reese.rifredi@gmail.com>
2023-02-16 16:48:50 -0800

    deleted js restriction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// let password;
-// let pass1 = "reese+leah";
-// password = prompt("What is the password?", " ");
-// if (password == pass1) {
-//     alert("Correct Password, you may enter!")
-// }
-// else {
-//     window.location="https://www.google.com"
-// }
+let password;
+let pass1 = "reese+leah";
+password = prompt("What is the password?", " ");
+if (password == pass1) {
+    alert("Correct Password, you may enter!")
+}
+else {
+    window.location="https://www.google.com"
+}
